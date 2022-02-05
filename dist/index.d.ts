1b033db4124547be561b1d54e45dc1b5e0182bc7
Seokseong Jeon <sjeon87+fork@gmail.com>
2022-02-05 23:10:07 +0900

    prototype

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import { SimulationNodeDatum } from "d3";
+export declare function collideRect(dim?: any): {
+    (): void;
+    initialize(_nodes: SimulationNodeDatum[], _random: Function): void;
+    iterations(_: any): number | any;
+    strength(_: any): number | any;
+    dim(_: any): any;
+};
+//# sourceMappingURL=index.d.ts.map
